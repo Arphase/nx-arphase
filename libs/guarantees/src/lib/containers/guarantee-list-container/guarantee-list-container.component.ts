bf11b74a446db764a2cc03232a1f5d5d6181549f
Víctor Martínez <victor.martinez@arphase.com>
2020-07-31 11:35:44 -0500

    feat(ui): add virtual scroll component

--- libs/guarantees/src/lib/containers/guarantee-list-container/guarantee-list-container.component.ts
+++ libs/guarantees/src/lib/containers/guarantee-list-container/guarantee-list-container.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ivt-guarantee-list-container',
   templateUrl: './guarantee-list-container.component.html',
   styleUrls: ['./guarantee-list-container.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GuaranteeListContainerComponent implements OnInit {
-  list = [{}];
-  constructor() { }
+  list = [];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
