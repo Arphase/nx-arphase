6cb3b39a6598ca2fd899dc2453f896d087895b0e
Víctor Martínez <victorandresmtz@gmail.com>
2020-08-07 11:15:37 -0500

    feat(api): add custom configuration for migrations

---
+++ apps/innovatech-api/src/db/config/ormconfig.ts
@@ -0,0 +1,25 @@
+import { ConnectionOptions } from 'typeorm';
+
+import { ENTITIES } from './entities';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  database: 'ivt_dev',
+  entities: ENTITIES,
+
+  // We are using migrations, synchronize should be set to false.
+  synchronize: false,
+  // Allow both start:prod and start:dev to use migrations
+  // __dirname is either dist or src folder, meaning either
+  // the compiled js in prod or the ts in dev.
+  migrations: ['apps/innovatech-api/src/db/migrations/**/*.js'],
+  cli: {
+    // Location of migration should be inside src folder
+    // to be compiled into dist/ folder.
+    migrationsDir: 'apps/innovatech-api/src/db/migrations',
+  },
+};
+
+export = config;
