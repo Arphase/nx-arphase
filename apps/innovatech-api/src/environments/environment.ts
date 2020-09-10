34300a37a9db3c36e7e0693e598bd02e49550905
Víctor Martínez <victor.martinez@arphase.com>
2020-09-10 11:23:53 -0500

    docs(app): update README

--- apps/innovatech-api/src/environments/environment.ts
+++ apps/innovatech-api/src/environments/environment.ts
@@ -8,8 +8,8 @@ export const environment: Environment = {
     port: 5432,
     database: 'innovatech_dev',
     synchronize: true,
-    username: '',
-    password: ''
+    username: 'innovatech',
+    password: 'innovatech'
   },
   server: {
     port: 3333,
