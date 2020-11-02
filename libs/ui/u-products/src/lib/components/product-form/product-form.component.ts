5c64e3b8d49db0847fc18f49c8b021a06f520154
Santiago Limas <santiago@kinedu.com>
2020-11-02 17:47:15 -0600

    minor fix in user.entity.ts

--- libs/ui/u-products/src/lib/components/product-form/product-form.component.ts
+++ libs/ui/u-products/src/lib/components/product-form/product-form.component.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component} from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Product } from '@ivt/c-data';
 import { IvtFormComponent } from '@ivt/u-ui';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
 
 
 @Component({
@@ -14,7 +13,7 @@ import { Observable, Subject } from 'rxjs';
 })
 
 export class ProductFormComponent extends IvtFormComponent<Product> {
-  selectedFile: File = null;
+  
   constructor(private fb: FormBuilder, private http: HttpClient) { 
     super();
     this.form = this.fb.group({
@@ -24,35 +23,5 @@ export class ProductFormComponent extends IvtFormComponent<Product> {
       logo: [null, Validators.required],
       template: [null, Validators.required]
     })
-  }
-
-  onFileSelected() {
-    this.form.get("logo").valueChanges.subscribe(selectedValue  => {
-      this.selectedFile = selectedValue
-    })
-  }
-
-  readFileAsDataUrl(file: File): Observable<string> {
-    const subject = new Subject<string>();
-    const reader = new FileReader();
-  
-    reader.onload = () => subject.next(reader.result as string);
-    reader.onerror = error => subject.error(error);
-    reader.onloadend = () => subject.complete();
-    reader.readAsDataURL(file);
-  
-    return subject.asObservable();
-  }
-
-  onUpload() {
-    const fd = new FormData();
-    this.readFileAsDataUrl(this.selectedFile)
-    fd.append('image', this.selectedFile, this.selectedFile.name)
-
-    //endpoint
-    this.http.post('url', fd)
-      .subscribe(res => {
-        console.log(res);
-      });
   }
 }
