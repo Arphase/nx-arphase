52d9d9d4fdd5bf7fd6404c44d6d1b17fe80e5dee
David Martinez <davidmtzv14@gmail.com>
2020-11-18 09:25:54 -0600

    refactor(app): Remove duplicate imports

--- libs/api/a-guarantees/src/lib/dto/create-dtos/create-vehicle.dto.ts
+++ libs/api/a-guarantees/src/lib/dto/create-dtos/create-vehicle.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateVehicleDto {
   @IsNotEmpty()
