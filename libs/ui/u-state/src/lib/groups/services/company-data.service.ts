1ec9346e223c3cfd450583b7043898506f2511d4
Víctor Martínez <victor.martinez@arphase.com>
2020-11-18 19:29:08 -0600

    refactor(app): change state imports

--- libs/ui/u-state/src/lib/groups/services/company-data.service.ts
+++ libs/ui/u-state/src/lib/groups/services/company-data.service.ts
@@ -1,23 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import { Company, GuaranteeSummary } from '@ivt/c-data';
-import { saveFile } from '@ivt/c-utils';
+import { Company } from '@ivt/c-data';
 import { HttpUrlGenerator } from '@ngrx/data';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 
 import { IvtDataService } from '../../core';
-import { IVT_STATE_CONFIGURATION, IvtStateConfiguration } from '../../state-config';
+import { IVT_UI_STATE_CONFIGURATION, IvtUiStateConfiguration } from '../../ui-state-config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CompanyDataService extends IvtDataService<Company> {
-
   constructor(
     protected http: HttpClient,
     protected httpUrlGenerator: HttpUrlGenerator,
-    @Inject(IVT_STATE_CONFIGURATION) public config: IvtStateConfiguration
+    @Inject(IVT_UI_STATE_CONFIGURATION) public config: IvtUiStateConfiguration
   ) {
     super('Company', http, httpUrlGenerator, config);
     this.entityUrl = `${this.config.apiUrl}/groups/`;
--- libs/ui/u-state/src/lib/groups/services/group-data.service.ts
+++ libs/ui/u-state/src/lib/groups/services/group-data.service.ts
@@ -1,23 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import { Group, GuaranteeSummary } from '@ivt/c-data';
-import { saveFile } from '@ivt/c-utils';
+import { Group } from '@ivt/c-data';
 import { HttpUrlGenerator } from '@ngrx/data';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 
 import { IvtDataService } from '../../core';
-import { IVT_STATE_CONFIGURATION, IvtStateConfiguration } from '../../state-config';
+import { IVT_UI_STATE_CONFIGURATION, IvtUiStateConfiguration } from '../../ui-state-config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GroupDataService extends IvtDataService<Group> {
-
   constructor(
     protected http: HttpClient,
     protected httpUrlGenerator: HttpUrlGenerator,
-    @Inject(IVT_STATE_CONFIGURATION) public config: IvtStateConfiguration
+    @Inject(IVT_UI_STATE_CONFIGURATION) public config: IvtUiStateConfiguration
   ) {
     super('Group', http, httpUrlGenerator, config);
     this.entityUrl = `${this.config.apiUrl}/groups/`;
