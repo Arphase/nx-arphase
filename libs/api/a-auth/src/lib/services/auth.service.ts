0cd7652c2e1af9941a699e357fa57b8b45a08bd9
David Martinez <davidmtzv14@gmail.com>
2020-11-30 17:44:31 -0600

    refactor(auth): Add styles & content to set password mail

--- libs/api/a-auth/src/lib/services/auth.service.ts
+++ libs/api/a-auth/src/lib/services/auth.service.ts
@@ -1,4 +1,10 @@
-import { ResetPasswordEntity, ResetPasswordRepository, UserRepository } from '@ivt/a-state';
+import {
+  IMAGE_ASSETS_PATH,
+  ResetPasswordEntity,
+  ResetPasswordRepository,
+  tobase64,
+  UserRepository,
+} from '@ivt/a-state';
 import { ResetPassword, User } from '@ivt/c-data';
 import {
   ForbiddenException,
@@ -102,15 +108,123 @@ export class AuthService {
           pass: process.env.MAIL_PASS,
         },
       });
+      console.log(__dirname + '/src/assets/img/logo.png');
 
       const mailOptions = {
         from: `Company <${process.env.MAIL_ACCOUNT_SENDER}>`,
         to: email,
         subject: 'Asignar Contraseña',
         text: 'Asignar contraseña',
-        html: `Hola! <br><br> En el siguiente link podrás asignar tu contraseña<br><br>
-          <a href="${process.env.MAIL_HOST_URL}/${tokenEntity.passwordToken}/${email}">
-          Asignar Contraseña</a>`,
+        html: `
+        <!doctype html>
+        <html lang="en-US">
+
+        <head>
+          <meta content="text/html; charset=utf-8"
+                http-equiv="Content-Type" />
+          <title>Crear contraseña</title>
+          <meta name="description"
+                content="Reset Password Email Template.">
+          <style type="text/css">
+            a:hover {
+              text-decoration: underline !important;
+            }
+          </style>
+        </head>
+
+        <body marginheight="0"
+              topmargin="0"
+              marginwidth="0"
+              style="margin: 0px; background-color: #f2f3f8;"
+              leftmargin="0">
+          <!--100% body table-->
+          <table cellspacing="0"
+                border="0"
+                cellpadding="0"
+                width="100%"
+                bgcolor="#f2f3f8"
+                style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+            <tr>
+              <td>
+                <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;"
+                      width="100%"
+                      border="0"
+                      align="center"
+                      cellpadding="0"
+                      cellspacing="0">
+                  <tr>
+                    <td style="height:80px;">&nbsp;</td>
+                  </tr>
+                  <tr>
+                    <td style="height:20px;">&nbsp;</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <table width="95%"
+                            border="0"
+                            align="center"
+                            cellpadding="0"
+                            cellspacing="0"
+                            style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                        <tr>
+                          <td style="height:40px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                          <td style="padding:0 35px;">
+                            <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">
+                              ¡Bienvenido a Innovatech!
+                            </h1>
+                            <span
+                                  style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                            <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                              Has recibido una invitación para entrar a la plataforma de Innovatech. Para crear la contraseña
+                              para tu cuenta has click en el siguiente link.
+                            </p>
+                            <a href="${process.env.MAIL_HOST_URL}/${tokenEntity.passwordToken}/${email}"
+                              style="background:#3f5378;text-decoration:none !important; font-weight:500; margin-top:35px; margin-bottom:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">
+                              Crear contrseña
+                            </a>
+                            <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                              Si el link no funciona copia y pega lo siguiente en tu navegador: ${process.env.MAIL_HOST_URL}/${tokenEntity.passwordToken}/${email}
+                            </p>
+                            <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                              Si no eres parte de uno de los grupos de Innovatech ignora este correo.
+                            </p>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="height:40px;">&nbsp;</td>
+                        </tr>
+                      </table>
+                    </td>
+                  <tr>
+                    <td style="height:20px;">&nbsp;</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align:center;">
+                      <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">
+                        &copy; <strong>www.innovatechcorp.com</strong></p>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style="height:80px;">&nbsp;</td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+          <!--/100% body table-->
+        </body>
+
+        </html>
+        `,
+        // attachments: [
+        //   {
+        //     filename: 'logo.png',
+        //     path: __dirname + '${IMAGE_ASSETS_PATH}logo.png',
+        //     cid: 'logo',
+        //   },
+        // ],
       };
 
       const sended = await new Promise<boolean>(async function (resolve, reject) {
