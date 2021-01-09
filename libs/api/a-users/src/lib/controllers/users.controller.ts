90fe31b940bbbfabcc43f454d2744a35f53d3230
Víctor Martínez <victor.martinez@arphase.com>
2021-01-09 11:45:16 -0600

    feat(users): get with query endpoint

--- libs/api/a-users/src/lib/controllers/users.controller.ts
+++ libs/api/a-users/src/lib/controllers/users.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { GetUser } from '@ivt/a-auth';
+import { User } from '@ivt/c-data';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 import { UsersService } from '../services/users.service';
@@ -7,4 +9,9 @@ import { UsersService } from '../services/users.service';
 @UseGuards(AuthGuard())
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @Get()
+  async getUsers(@GetUser() user: Partial<User>): Promise<User[]> {
+    return this.usersService.getUsers(user);
+  }
 }
--- libs/api/a-users/src/lib/services/users.service.ts
+++ libs/api/a-users/src/lib/services/users.service.ts
@@ -1,4 +1,5 @@
 import { UserRepository } from '@ivt/a-state';
+import { User, UserRoles } from '@ivt/c-data';
 import { Injectable } from '@nestjs/common';
 import { Connection } from 'typeorm';
 
@@ -8,5 +9,14 @@ export class UsersService {
 
   constructor(private readonly connection: Connection) {
     this.userRepository = this.connection.getCustomRepository(UserRepository);
+  }
+
+  async getUsers(user: Partial<User>): Promise<User[]> {
+    const query = this.userRepository.createQueryBuilder('user');
+    if (user && UserRoles[user.role] !== UserRoles.superAdmin) {
+      query.andWhere('(user.companyId = :id)', { id: user.companyId });
+    }
+
+    return await query.getMany();
   }
 }
