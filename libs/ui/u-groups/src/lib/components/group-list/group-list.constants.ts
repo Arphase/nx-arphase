e523210aec29f77fee6c2e879921e607fd02e223
Víctor Martínez <victor.martinez@arphase.com>
2021-01-13 13:10:19 -0600

    feat(groups): mobile responsiveness

--- libs/ui/u-groups/src/lib/components/group-list/group-list.constants.ts
+++ libs/ui/u-groups/src/lib/components/group-list/group-list.constants.ts
@@ -5,29 +5,45 @@ export const columns: IvtColumns = [
     label: 'Nombre',
     prop: 'group.name',
     sortable: false,
+    colSizes: {
+      xs: '3'
+    }
   },
   {
     label: 'Contacto',
     prop: 'group.contact',
     sortable: false,
+    colSizes: {
+      xs: '3',
+    },
+    breakpointShow: {
+      md: true,
+      lg: true,
+      xl: true,
+    },
   },
   {
     label: 'Email',
     prop: 'group.email',
     sortable: false,
+    colSizes: {
+      xs: '3',
+      md: '2'
+    },
   },
   {
     label: 'Teléfono',
     prop: 'group.phone',
     sortable: false,
+    colSizes: {
+      xs: '3',
+      md: '2'
+    },
   },
   {
     label: 'Acciones',
     prop: 'actions',
     sortable: false,
-    alignment: 'right',
-    colSizes: {
-      xs: 'auto',
-    },
+    alignment: 'right'
   },
 ];
