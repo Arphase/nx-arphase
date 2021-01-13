fe0d19adc4b4075ff17eaf409eecdbdef373d401
Víctor Martínez <victor.martinez@arphase.com>
2021-01-13 14:28:31 -0600

    feat(app): add application version

--- apps/innovatech-ui/src/environments/environment.ts
+++ apps/innovatech-ui/src/environments/environment.ts
@@ -1,10 +1,12 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import * as packageJson from '../../../../package.json';
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3333'
+  apiUrl: 'http://localhost:3333',
+  version: packageJson.version,
 };
 
 /*
