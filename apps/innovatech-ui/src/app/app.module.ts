cefe1db3c08955ab88d979bfbab26a216491ac6a
Víctor Martínez <victor.martinez@arphase.com>
2021-01-16 12:45:09 -0600

    feat(app): add PWA support

--- apps/innovatech-ui/src/app/app.module.ts
+++ apps/innovatech-ui/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { IVT_UI_STATE_CONFIGURATION, IvtStateModule, IvtUiStateConfiguration } from '@ivt/u-state';
 import { IvtUiModule } from '@ivt/u-ui';
 import { NgxMaskModule } from 'ngx-mask';
@@ -27,6 +28,7 @@ const IVT_STATE_CONFIGURATION_VALUE: IvtUiStateConfiguration = {
     IvtUiModule,
     NgxMaskModule.forRoot(),
     ToastrModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [
     {
