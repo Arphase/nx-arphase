e03cf7fad6a2dcd1dad8c4401cc1fc0fb4620f20
Víctor Martínez <victor.martinez@arphase.com>
2021-01-23 14:53:30 -0600

    build(api): add sentry

--- apps/innovatech-api/src/environments/environment.model.ts
+++ apps/innovatech-api/src/environments/environment.model.ts
@@ -12,4 +12,5 @@ export interface Environment {
   server: {
     port: number;
   };
+  environmentName: string;
 }
--- apps/innovatech-api/src/main.ts
+++ apps/innovatech-api/src/main.ts
@@ -3,20 +3,29 @@
  * This is only a minimal backend to get started.
  */
 import { environment } from '@api/env/environment';
+import { SentryInterceptor } from '@ivt/a-state';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as Sentry from '@sentry/node';
+import * as bodyParser from 'body-parser';
 
 import { AppModule } from './app/app.module';
-import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.use(bodyParser.json({limit: '50mb'}));
-  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+  app.useGlobalInterceptors(new SentryInterceptor());
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
   app.enableCors();
   const port = process.env.PORT || environment.server.port;
+
+  Sentry.init({
+    dsn: 'https://5ef3606696264220b6259afebdd0a635@o509882.ingest.sentry.io/5604926',
+    environment: environment.environmentName,
+  });
+
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port);
   });
