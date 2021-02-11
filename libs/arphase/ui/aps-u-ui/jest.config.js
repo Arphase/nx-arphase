08849de2bee30be2fbe2bb5a6848d246f232c75e
Víctor Martínez <victor.martinez@arphase.com>
2021-02-10 18:48:41 -0600

    feat(arphase:ui): create library

---
+++ libs/arphase/ui/aps-u-ui/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  displayName: 'arphase-ui-aps-u-ui',
+  preset: '../../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
+  coverageDirectory: '../../../../coverage/libs/arphase/ui/aps-u-ui',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
