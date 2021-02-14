37c4f83c594bdd4f56eef1dd7aaadc4653501945
Víctor Martínez <victor.martinez@arphase.com>
2021-02-13 18:44:24 -0600

    refactor(api): add db migration

---
+++ apps/innovatech-api/src/db/migrations/1613263422756-NullableDistributor.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class NullableDistributor1613263422756 implements MigrationInterface {
+    name = 'NullableDistributor1613263422756'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "paymentOrders" ALTER COLUMN "distributor" DROP NOT NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "paymentOrders"."distributor" IS NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`COMMENT ON COLUMN "paymentOrders"."distributor" IS NULL`);
+        await queryRunner.query(`ALTER TABLE "paymentOrders" ALTER COLUMN "distributor" SET NOT NULL`);
+    }
+
+}
--- apps/innovatech-api/src/environments/environment.ts
+++ apps/innovatech-api/src/environments/environment.ts
@@ -4,12 +4,12 @@ export const environment: Environment = {
   production: false,
   databaseConfig: {
     type: 'postgres',
-    host: 'localhost',
+    host: 'aa6i74lcy90hh9.ckvaqjgmdubs.us-east-1.rds.amazonaws.com',
     port: 5432,
-    database: 'innovatech_dev',
-    synchronize: true,
-    username: 'innovatech',
-    password: 'innovatech',
+    database: 'ebdb',
+    synchronize: false,
+    username: 'innovatechqa',
+    password: 'Innovatech123',
   },
   server: {
     port: 3333,
