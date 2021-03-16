cb7a748b04868fb0776054cd6cc82e7a768fb7d6
Víctor Martínez <victor.martinez@arphase.com>
2021-03-15 18:16:05 -0600

    feat(payment-orders): create and update only for super admin users

--- libs/ui/u-guarantees/src/lib/components/guarantee-list/guarantee-list.component.ts
+++ libs/ui/u-guarantees/src/lib/components/guarantee-list/guarantee-list.component.ts
@@ -22,6 +22,7 @@ import { colorMaps, columns, iconMaps, statusOptions } from './guarantee-list.co
 })
 export class GuaranteeListComponent extends IvtListComponent<Guarantee> implements OnChanges {
   @Input() clearSelected: boolean;
+  @Input() canModifyPaymentOrders: boolean;
   dateTypeOptions = guaranteeDateTypeOptions;
   statusOptions = statusOptions;
   checked = false;
@@ -43,7 +44,7 @@ export class GuaranteeListComponent extends IvtListComponent<Guarantee> implemen
   }
 
   get showPaymentOrderButton(): boolean {
-    return this.checked || this.indeterminate;
+    return (this.checked || this.indeterminate) && this.canModifyPaymentOrders;
   }
 
   ngOnChanges(changes: SimpleChanges) {
