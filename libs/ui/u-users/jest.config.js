4454e141949e4507432974fa37a3c5a3528213de
Víctor Martínez <victor.martinez@arphase.com>
2021-04-23 11:52:16 -0500

    test(u-users): pass unit tests

--- libs/ui/u-users/jest.config.js
+++ libs/ui/u-users/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'ui-u-users',
+  displayName: 'u-users',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
+      tsconfig: '<rootDir>/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.(html|svg)$',
       astTransformers: {
         before: [
@@ -14,10 +14,10 @@ module.exports = {
       },
     },
   },
-  coverageDirectory: '../../../coverage/libs/ui/u-users',
+  coverageDirectory: '../../../coverage/libs/u-users',
   snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
   ],
 };
--- libs/ui/u-users/src/lib/components/user-list/user-list.component.spec.ts
+++ libs/ui/u-users/src/lib/components/user-list/user-list.component.spec.ts
@@ -1,25 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IvtEmptyPipe } from '@ivt/u-ui';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { MockPipe } from 'ng-mocks';
 
 import { UserListComponent } from './user-list.component';
 
 describe('UserListComponent', () => {
-  let component: UserListComponent;
-  let fixture: ComponentFixture<UserListComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
-    })
-    .compileComponents();
+  let spectator: Spectator<UserListComponent>;
+  const createComponent = createComponentFactory({
+    component: UserListComponent,
+    declarations: [MockPipe(IvtEmptyPipe)],
+    shallow: true,
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
+  beforeEach(() => (spectator = createComponent()));
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
   });
 });
