03803fa4bc6519752240ac93e5ff9c781fe98bc7
Víctor Martínez <victor.martinez@arphase.com>
2021-06-05 12:48:51 -0500

    build(app): update to Angular 12

--- apps/innovatech/app/src/environments/environment.ts
+++ apps/innovatech/app/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import * as packageJson from '../../../../../package.json';
+import packageJson from '../../../../../package.json';
 import { Environment } from './environment.model';
 
 export const environment: Environment = {
