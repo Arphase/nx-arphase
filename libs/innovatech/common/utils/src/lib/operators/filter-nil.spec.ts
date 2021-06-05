5f46a5226058ed315442f24db5ddc8a4c2017fdd
Víctor Martínez <victor.martinez@arphase.com>
2021-06-05 16:15:42 -0500

    refactor(app): fix unit tests

--- libs/innovatech/common/utils/src/lib/operators/filter-nil.spec.ts
+++ libs/innovatech/common/utils/src/lib/operators/filter-nil.spec.ts
@@ -1,7 +1,12 @@
-import { cold } from 'jasmine-marbles';
+import { addMatchers, cold, initTestScheduler } from 'jasmine-marbles';
+
 import { filterNil } from './filter-nil';
 
 describe('FilterNil Operator', () => {
+  beforeEach(() => {
+    initTestScheduler();
+    addMatchers();
+  });
   it('should filter null and/or undefined values out of the stream', () => {
     const values = { a: null, b: 0, c: '', d: undefined, e: true };
 
