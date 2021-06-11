49a6d2a8f885d41614f5ecd8af6dea2fd004e2d4
David Martinez <david.martinez@interfactura.com>
2021-06-11 16:07:16 -0500

    test(musicr/app): Remove tests

--- apps/musicr/admin/src/app/app.component.spec.ts
+++ apps/musicr/admin/src/app/app.component.spec.ts
@@ -16,16 +16,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'musicr-admin'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('musicr-admin');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to musicr-admin!');
-  });
 });
