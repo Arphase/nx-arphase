9150b62179d3c463af32e64bac5c9164a8a4b86d
Dario Salazar <dario.salazar@interfactura.com>
2021-08-23 15:10:03 -0500

    feat(valmira/frequent-questions): #121 add frequent-questions images and header

--- apps/valmira/app/src/app/app.module.ts
+++ apps/valmira/app/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { icons } from './icons';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, NzIconModule.forRoot(icons)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- libs/valmira/ui/frequent-questions/src/lib/frequent-questions.component.ts
+++ libs/valmira/ui/frequent-questions/src/lib/frequent-questions.component.ts
@@ -6,4 +6,32 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./frequent-questions.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FrequentQuestionsComponent {}
+export class FrequentQuestionsComponent {
+  pageTilte = 'frequent questions';
+  questions: unknown[] = [
+    {
+      "question": "¿Puedo invitar personas adicionales a mi alojamiento?",
+      "answer": "Para asegurar que todos los huéspedes disfruten su estancia al 100% no se podrá exceder la capacidad máxima de cada alojamiento.",
+    },
+    {
+      "question": "¿Pueden ir niños?",
+      "answer": "¡Claro! Valmira ofrece opciones que se adecúan a cada huésped. En el caso de los niños, la mejor opción son los alojamientos de la categoría “Familiar”.",
+    },
+    {
+      "question": "¿Puedo traer a mi mascota?",
+      "answer": "Sabemos que tu alojamiento no podría estar completo sin tu mascota, ¡así que estamos preparados para recibirlas!",
+    },
+    {
+      "question": "¿Qué tan seguros son los alojamientos?",
+      "answer": "Los alojamientos siguen protocolos estrictos para que tú y tus acompañantes puedan disfrutar sin riesgos. Y aún ante cualquier imprevisto, el lobby está preparado con botiquín, y demás elementos de seguridad.",
+    },
+    {
+      "question": "¿Hay estacionamiento suficiente?",
+      "answer": "Por supuesto, los cajones están asignados de acuerdo a la cantidad de huéspedes establecida para cada alojamiento: 1 cajón para los alojamientos de la categoría “Pareja” y dos cajones para “Familiar”.",
+    },
+    {
+      "question": "¿Dónde están ubicados?",
+      "answer": "Valmira se encuentra ubicado en Valle Venado Safari, en la zona campestre de Montemorelos. A 55 minutos de la ciudad de Monterrey, N.L.",
+    },
+  ];
+}
