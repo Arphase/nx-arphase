242d94332c98525f443b9cf81cf2ae9316e697ff
Daniel Martinez <danymtzv14@gmail.com>
2021-08-31 10:22:50 -0500

    feat(valmira/landing-page): #118 Unit test fixes

--- libs/valmira/ui/landing/src/lib/landing.component.spec.ts
+++ libs/valmira/ui/landing/src/lib/landing.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { LandingComponent } from './landing.component';
 
 describe('LandingComponent', () => {
@@ -8,7 +8,8 @@ describe('LandingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LandingComponent ]
+      declarations: [ LandingComponent ],
+      schemas:[NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
