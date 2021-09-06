509aa27dfe61da97a761aa7498ba424f90b9b616
Víctor Martínez <victor.martinez@arphase.com>
2021-09-05 19:02:55 -0500

    fix(innovatech/payment-orders): create dto missing fields

--- libs/innovatech/api/payment-orders/src/lib/dto/create-payment-order.dto.ts
+++ libs/innovatech/api/payment-orders/src/lib/dto/create-payment-order.dto.ts
@@ -1,7 +1,24 @@
 import { Guarantee } from '@innovatech/common/domain';
-import { IsArray } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsArray, IsDate, IsNumber, IsNumberString, IsString, ValidateNested } from 'class-validator';
 
 export class CreatePaymentOrderDto {
   @IsArray()
+  @ValidateNested()
+  @Type(() => CreatePaymentOrderGuaranteeDto)
   guarantees: Partial<Guarantee>[];
+}
+
+export class CreatePaymentOrderGuaranteeDto {
+  @IsNumber()
+  id: number;
+
+  @IsNumberString()
+  amount: string;
+
+  @IsDate()
+  invoiceDate: Date;
+
+  @IsString()
+  invoiceNumber: string;
 }
