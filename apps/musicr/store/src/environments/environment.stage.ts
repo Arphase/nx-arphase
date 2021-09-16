ab56148361070dff6aa1f7da0354562d773d170d
Víctor Martínez <victor.martinez@arphase.com>
2021-09-16 11:58:28 -0500

    refactor(app): fix package.json

--- apps/musicr/store/src/environments/environment.stage.ts
+++ apps/musicr/store/src/environments/environment.stage.ts
@@ -3,7 +3,7 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: true,
-  apiUrl: 'https://api.musicrevolution.com',
+  apiUrl: 'https://apistage.musicrevolution.com',
   version: packageJson.version,
-  environmentName: 'prod',
+  environmentName: 'stage',
 };
