c9f31b8e7a9183f92119eb2a41b3df2752e66870
Daniel Martinez <danymtzv14@gmail.com>
2021-09-21 12:22:54 -0500

    feat(valmira/manifest): #120 Manifest Page

--- libs/valmira/ui/landing/src/lib/landing.module.ts
+++ libs/valmira/ui/landing/src/lib/landing.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 
-
 @NgModule({
-  imports: [CommonModule, LandingRoutingModule, NzLayoutModule, NzMenuModule, NzGridModule, NzButtonModule],
+  imports: [CommonModule, LandingRoutingModule, NzLayoutModule, NzGridModule, NzButtonModule],
   declarations: [LandingComponent],
 })
 export class LandingModule {}
