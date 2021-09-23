017623ed010db2fd4b5e64f7100bec5f3be02a39
Víctor Martínez <victor.martinez@arphase.com>
2021-09-23 18:49:32 -0500

    refactor(app): fix unit tests

--- libs/valmira/ui/reservation-wizard/src/lib/containers/reservation-wizard-container/reservation-wizard-container.component.spec.ts
+++ libs/valmira/ui/reservation-wizard/src/lib/containers/reservation-wizard-container/reservation-wizard-container.component.spec.ts
@@ -1,14 +1,22 @@
 import { RouterTestingModule } from '@angular/router/testing';
 import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Action } from '@ngrx/store';
+import { provideMockStore } from '@ngrx/store/testing';
 import { ReservationCollectionService } from '@valmira/ui/reservations/data';
+import { Observable } from 'rxjs';
 
 import { ReservationWizardContainerComponent } from './reservation-wizard-container.component';
 
 describe('ReservationWizardContainerComponent', () => {
   let spectator: Spectator<ReservationWizardContainerComponent>;
+  // eslint-disable-next-line prefer-const
+  let actions$ = new Observable<Action>();
+
   const createComponent = createComponentFactory({
     component: ReservationWizardContainerComponent,
     imports: [RouterTestingModule],
+    providers: [provideMockStore(), provideMockActions(() => actions$)],
     mocks: [ReservationCollectionService],
     shallow: true,
   });
