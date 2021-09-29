7f24abbb6a87d5d25824072873cdeda95f7a4d73
Daniel Martinez <danymtzv14@gmail.com>
2021-09-29 16:03:09 -0500

    WIP

--- libs/valmira/ui/core/src/lib/footer/footer.module.ts
+++ libs/valmira/ui/core/src/lib/footer/footer.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
-
-
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
-  declarations: [
-    FooterComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    FooterComponent
-  ]
+  declarations: [FooterComponent],
+  imports: [CommonModule, NzLayoutModule, NzGridModule, NzButtonModule],
+  exports: [FooterComponent],
 })
-export class FooterModule { }
+export class FooterModule {}
