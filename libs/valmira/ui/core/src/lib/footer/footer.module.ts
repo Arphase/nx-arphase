2a07a031339d3d12d0e7c4006c27979cba568935
Víctor Martínez <victor.martinez@arphase.com>
2021-10-01 15:57:06 -0500

    feat(valmira/landing): apply responsive styles

--- libs/valmira/ui/core/src/lib/footer/footer.module.ts
+++ libs/valmira/ui/core/src/lib/footer/footer.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer.component';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+import { FooterComponent } from './footer.component';
 
 @NgModule({
   declarations: [FooterComponent],
-  imports: [CommonModule, RouterModule, NzLayoutModule, NzGridModule, NzButtonModule],
+  imports: [CommonModule, RouterModule, NzGridModule, NzButtonModule, NzIconModule],
   exports: [FooterComponent],
 })
 export class FooterModule {}
