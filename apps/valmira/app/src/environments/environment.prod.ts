0b0761c7a98210ac4584f4f5c97aeec0a36bffb1
Víctor Martínez <victor.martinez@arphase.com>
2021-10-08 19:35:24 -0500

    build(app): limit memory to 2GB in build time

--- apps/valmira/app/src/environments/environment.prod.ts
+++ apps/valmira/app/src/environments/environment.prod.ts
@@ -1,11 +1,7 @@
-import packageJson from '../../../../../package.json';
-import { Environment } from './environment.model';
-
-export const environment: Environment = {
-  production: true,
-  apiUrl: '/api',
-  version: packageJson.version,
-  environmentName: 'prod',
-  stripeKey:
-    'pk_live_51JV2c7ESPeV0d6H2k9moacez9zL2t5U5kIbWPqyHmFtaMz10cCWAPYhT0ojOGuThZ9kplXP6dD2amFLtJXPyvlni00vG3WRZuU',
+export const environment = {
+   production: false,
+   apiUrl: 'undefined',
+   version: '3.5.4',
+   environmentName: 'undefined',
+   stripeKey: 'undefined'
 };
