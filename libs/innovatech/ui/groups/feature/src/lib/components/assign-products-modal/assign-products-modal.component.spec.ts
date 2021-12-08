e45017f9166842b324354ce1c4698082f2e24832
Víctor Martínez <victor.martinez@arphase.com>
2021-12-08 12:24:30 -0600

    refactor(app): fix unit tests

--- libs/innovatech/ui/groups/feature/src/lib/components/assign-products-modal/assign-products-modal.component.spec.ts
+++ libs/innovatech/ui/groups/feature/src/lib/components/assign-products-modal/assign-products-modal.component.spec.ts
@@ -1,4 +1,6 @@
+import { ApsEmptyPipe } from '@arphase/ui/core';
 import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { MockPipe } from 'ng-mocks';
 
 import { AssignProductsModalComponent } from './assign-products-modal.component';
 
@@ -6,6 +8,7 @@ describe('AssignProductsModalComponent', () => {
   let spectator: Spectator<AssignProductsModalComponent>;
   const createComponent = createComponentFactory({
     component: AssignProductsModalComponent,
+    declarations: [MockPipe(ApsEmptyPipe)],
     shallow: true,
   });
 
