4ed4591ceceb40d6a1225315b895c0088d4ad317
Víctor Martínez <victor.martinez@arphase.com>
2022-03-29 21:28:25 -0600

    refactor(musicr/api): change ormconfig migration files path

--- libs/musicr/api/db/src/lib/config/ormconfig.ts
+++ libs/musicr/api/db/src/lib/config/ormconfig.ts
@@ -17,17 +17,10 @@ export default {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   entities: ENTITIES,
-
-  // We are using migrations, synchronize should be set to false.
   synchronize: process.env.SYNCHRONIZE === 'true',
-  // Allow both start:prod and start:dev to use migrations
-  // __dirname is either dist or src folder, meaning either
-  // the compiled js in prod or the ts in dev.
-  migrations: ['libs/musicr/api/db/src/lib/migrations/**/*.js'],
   cli: {
-    // Location of migration should be inside src folder
-    // to be compiled into dist/ folder.
     migrationsDir: 'libs/musicr/api/db/src/lib/migrations',
   },
+  migrations: ['libs/musicr/api/db/src/lib/migrations/**/*.ts'],
   logging: !test,
 } as TypeOrmModuleOptions;
