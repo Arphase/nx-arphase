5423c132182d30bffe5ae3284ef66230543dd494
Víctor Martínez <victorandresmtz@gmail.com>
2022-12-12 21:31:40 -0600

    refactor(musicr/api): fix order status migration

--- libs/musicr/api/db/src/lib/migrations/1670610792923-AddStatusToOrder.ts
+++ libs/musicr/api/db/src/lib/migrations/1670610792923-AddStatusToOrder.ts
@@ -5,11 +5,15 @@ export class AddStatusToOrder1670610792923 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
+      `CREATE TYPE "public"."orders_status_enum" AS ENUM('quoted', 'notSpecified', 'done', 'inProcess')`
+    );
+    await queryRunner.query(
       `ALTER TABLE "orders" ADD "status" "public"."orders_status_enum" NOT NULL DEFAULT 'quoted'`
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "status"`);
+    await queryRunner.query(`DROP TYPE "public"."orders_status_enum"`);
   }
 }
