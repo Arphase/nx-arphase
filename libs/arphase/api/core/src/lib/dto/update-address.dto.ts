9e300d5a4ef282fcb974b07c3cc2039c185a388d
Víctor Martínez <victorandresmtz@gmail.com>
2023-06-12 16:31:51 -0600

    fix(core/api): don't require id when updating address

--- libs/arphase/api/core/src/lib/dto/update-address.dto.ts
+++ libs/arphase/api/core/src/lib/dto/update-address.dto.ts
@@ -1,9 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 
 import { CreateAddressDto } from './create-address.dto';
 
 export class UpdateAddressDto extends PartialType(CreateAddressDto) {
+  @IsOptional()
   @IsNumber()
   id: number;
 }
