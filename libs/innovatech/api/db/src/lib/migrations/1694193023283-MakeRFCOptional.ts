366841bad0b7744fb67fcc9ae09b7650a6d2ce34
Víctor Martínez <victorandresmtz@gmail.com>
2023-09-08 11:12:00 -0600

    refactor(innovatech/guarantees): add missing migration

---
+++ libs/innovatech/api/db/src/lib/migrations/1694193023283-MakeRFCOptional.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MakeRFCOptional1694193023283 implements MigrationInterface {
+    name = 'MakeRFCOptional1694193023283'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "clients" ALTER COLUMN "rfc" DROP NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "clients" ALTER COLUMN "rfc" SET NOT NULL`);
+    }
+
+}
