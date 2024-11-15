e49c2d14421207bead5225df774a1a9ec2f288ec
Víctor Martínez <victorandresmtz@gmail.com>
2024-11-15 09:15:56 -0600

    build(musicr/admin): change environment variables for production

--- apps/musicr/admin/src/environments/environment.prod.ts
+++ apps/musicr/admin/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: true,
-  apiUrl: 'https://musicrevolution.mx/api',
+  apiUrl: 'https://api.musicrevolution.mx',
   innovatechUrl: 'https://api.innovatechcorp.com',
   version: packageJson.version,
   environmentName: 'prod',
